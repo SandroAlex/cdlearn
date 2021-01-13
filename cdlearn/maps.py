420e146e71e00d88fb778483441445553fca24e5
Alex Araujo <alex.fate2000@gmail.com>
2021-01-13 12:45:17 -0300

    Add function for general maps

--- cdlearn/maps.py
+++ cdlearn/maps.py
@@ -40,11 +40,12 @@ def south_america(
         figsize=(8, 8),
         nrows_ncols=(1, 1),
         suptitle=None,
-		suptitle_y=0.90,
+        suptitle_y=0.90,
         titles=None,
         axes_pad=0.02,
         cbar_mode="single",
         cbar_location="right",
+        cbar_pad="",
         brazil_color="black"
     ):
     """
@@ -207,5 +208,107 @@ def south_america_months(
     # Title and further adjustments.
     plt.suptitle(title, weight="bold")
     plt.subplots_adjust(top=0.975)
+
+    return axgr
+
+###############################################################################
+def general(
+        figsize=(13, 6),
+        nrows_ncols=(1, 1),
+        region={"lati": -90, "latf": 90, "loni": -180, "lonf": 180},
+        region_grid={"lat": 15, "lon": 30},
+        dticks={"x": 2, "y": 2},
+        projection=ccrs.PlateCarree(),
+        axes_pad=0.02,
+        cbar_mode="single",
+        cbar_location="right",   
+        cbar_pad=0.50,
+        brazilian_states=False
+    ):
+    """
+    Custom map(s). By default, it draws one global map with cylindrical 
+    projection and an empty axis for colorbar. 
+
+    Parameters
+    ----------
+    figsize : tuple, optional, default is (13, 13)
+        Size for the whole figure.
+    nrows_ncols : tuple of ints, optional, default is (1, 1)
+        Grid of maps to be generated.         
+    region : dict, optional, default is for the whole Globe
+        Ranges for latitude and longitude of this map.   
+    region_grid : dict, optional, is 15 (latitude) and 30 (longitude)
+        Coordinate spacings for drawing grid.
+    dticks : dict, optional, default is 2 (for both x and y)
+        Units of grid spacing for ticks.    
+    projection : Cartopy's coordenate reference system
+        Map projection, cylindrical by default. 
+    axes_pad : float, optional, default is 0.02
+        Padding between axes.   
+    axes_pad : float, optional, default is 0.02
+        Padding between axes.    
+    cbar_mode : str, optional, default is "single"
+        Type of colorbar according to these options: "each", "single", "edge", 
+        or None.  
+    cbar_location : str, optional, default is "right"
+        Location of colorbar according to these options: "left", "right", 
+        "bottom", or "top". 
+    cbar_pad : float, optional, default is 0.25            
+        Padding between the image axes and the colorbar axes.
+
+    Returns
+    -------
+    axgr : mpl_toolkits.axes_grid1.axes_grid.ImageGrid object
+        Grid of axes.
+    """  
+
+    # Make grid object.    
+    axes_class = (GeoAxes, dict(map_projection=projection))
+    fig = plt.figure(figsize=figsize)
+    axgr = AxesGrid(
+        fig=fig, 
+        rect=111, 
+        axes_class=axes_class,
+        nrows_ncols=nrows_ncols,
+        axes_pad=axes_pad,
+        cbar_mode=cbar_mode,
+        cbar_location=cbar_location,
+        cbar_pad=cbar_pad,
+        cbar_size="3%",
+        label_mode=""
+    )
+
+    # Make detailed maps.
+    map_extent = [
+        region["loni"], region["lonf"], region["lati"], region["latf"]
+    ]
+    xticks = np.arange(
+        region["loni"], region["lonf"] + region_grid["lon"], region_grid["lon"]
+    )
+    yticks = np.arange(
+        region["lati"], region["latf"] + region_grid["lat"], region_grid["lat"]
+    ) 
+    for index, axis in enumerate(axgr):
+        axis.coastlines()
+        axis.set_extent(map_extent, crs=projection)
+        axis.add_feature(cfeature.BORDERS)
+        axis.set_xticks(xticks[::dticks["x"]], crs=projection)
+        axis.set_yticks(yticks[::dticks["y"]], crs=projection)
+        lon_formatter = LongitudeFormatter(zero_direction_label=True)
+        lat_formatter = LatitudeFormatter()
+        axis.xaxis.set_major_formatter(lon_formatter)
+        axis.yaxis.set_major_formatter(lat_formatter)
+        axis.gridlines(xlocs=xticks, ylocs=yticks)  
+
+        if brazilian_states:
+        
+            # Brazilian states's boundaries.
+            for state in shp.geometries():
+                axis.add_geometries(
+                    geoms=[state], 
+                    crs=projection, 
+                    facecolor="none", 
+                    edgecolor="black"
+                )
 
     return axgr
